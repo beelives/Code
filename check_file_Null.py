3d5930f780e603600c71b7b6b56a1267a87a2dd6
beelives <rbeelives@gmail.com>
2015-10-22 00:32:22 +0800

    add

---
+++ check_file_Null.py
@@ -0,0 +1,8 @@
+import os
+if os.stat(file_name).st_size != 0:
+    pass
+
+
+# 确认文件不为空
+# http://stackoverflow.com/questions/2507808/python-how-to-check-file-empty-or-not
+# http://stackoverflow.com/questions/14639936/how-to-read-from-file-opened-in-a-mode
